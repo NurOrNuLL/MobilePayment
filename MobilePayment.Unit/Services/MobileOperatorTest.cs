62673008aa93ac73792e9d1242121c5a8347551d
Nursu <nursultan.orynbayev@gmail.com>
2021-08-23 03:18:13 +0600

    Рефакторинг: замокал сервис для тестирование.

--- MobilePayment.Unit/Services/MobileOperatorTest.cs
+++ MobilePayment.Unit/Services/MobileOperatorTest.cs
@@ -1,14 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using MobilePayment.Application.Dtos;
 using MobilePayment.Application.Exception;
 using MobilePayment.Application.Services.MobileOperatorService;
 using MobilePayment.Application.Services.MobileOperatorService.Interfaces;
 using MobilePayment.Domain.Entities.Enums;
-using MobilePayment.Domain.Repositories;
 using MobilePayment.Unit.Fakes;
+using Moq;
 using Xunit;
 
 namespace MobilePayment.Unit.Services
@@ -16,33 +17,32 @@ namespace MobilePayment.Unit.Services
     public class MobileOperatorTest
     {
         private readonly IMobileOperatorStrategy _operatorStrategy;
+        private readonly Mock<ILogger<MobileOperatorStrategy>> _mockLogger = new();
 
         public MobileOperatorTest()
         {
-            var services = new ServiceCollection();
-
-            services.AddScoped<ITransactionRepository, FakeTransactionRepository>();
-            services.AddScoped<IMobileOperatorStrategy, MobileOperatorStrategy>();
-            services.AddScoped<IMobileOperator, FakeMobileOperatorThrowException>();
-            services.AddScoped<IMobileOperator, FakeMobileOperatorSuccess>();
-            services.AddScoped<IMobileOperator, FakeMobileOperatorFailure>();
-
-            var provider = services.BuildServiceProvider();
-            _operatorStrategy = (IMobileOperatorStrategy)provider.GetRequiredService(typeof(IMobileOperatorStrategy));
+            _operatorStrategy = new MobileOperatorStrategy(new List<IMobileOperator>
+            {
+                new FakeMobileOperatorFailure(),
+                new FakeMobileOperatorSuccess(),
+                new FakeMobileOperatorThrowException()
+            }, new FakeTransactionRepository(), _mockLogger.Object);
         }
 
         [Fact]
         public async Task Should_Get_Success_Result()
         {
-            var result = await _operatorStrategy.SendRequestAsync(ValidPayment.From(("1111111111", 200m)), OperatorType.Beeline);
+            var result =
+                await _operatorStrategy.SendRequestAsync(ValidPayment.From(("1111111111", 200m)), OperatorType.Beeline);
             result.Value.Type.Should().Be(OperatorType.Beeline);
             result.Value.Status.Should().Be(TransactionStatus.Success);
         }
-        
+
         [Fact]
         public async Task Should_Get_Failure_Result()
         {
-            var result = await _operatorStrategy.SendRequestAsync(ValidPayment.From(("1111111111", 200m)), OperatorType.Altel);
+            var result =
+                await _operatorStrategy.SendRequestAsync(ValidPayment.From(("1111111111", 200m)), OperatorType.Altel);
             result.Value.Type.Should().Be(OperatorType.Altel);
             result.Value.Status.Should().Be(TransactionStatus.Failure);
         }
@@ -50,7 +50,8 @@ namespace MobilePayment.Unit.Services
         [Fact]
         public async Task Should_Throw_MobileServerNotResponse_Exception()
         {
-            Func<Task> act = async () => await _operatorStrategy.SendRequestAsync(ValidPayment.From(("1111111111", 200m)), OperatorType.Active);
+            Func<Task> act = async () =>
+                await _operatorStrategy.SendRequestAsync(ValidPayment.From(("1111111111", 200m)), OperatorType.Active);
             await act.Should().ThrowAsync<MobileServerNotResponse>()
                 .WithMessage(nameof(FakeMobileOperatorThrowException));
         }
